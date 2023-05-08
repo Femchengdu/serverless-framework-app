5b55aff6baa6b19fe61bf4b6cc8d319d53345afe
Femi Ajayi <femiajayi705@gmail.com>
2023-05-08 13:28:14 +0200

    core: Added client app

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '7uyead9f1j'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-6x-sznpv.us.auth0.com',            // Auth0 domain
+  clientId: 'Kr7gFioY2inETHLJdRJ89FB3Bp8lQveu',   // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
